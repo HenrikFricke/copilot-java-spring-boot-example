302b73ae300515cad8d59aa966381710ed7eadd4
Alexander Steppke <alex@steppke.dev>
2022-06-08 17:33:21 +0200

    chore: switch to env native service discovery

--- apps/tutorials/src/main/java/com/superluminar/tutorials/client/EventClient.java
+++ apps/tutorials/src/main/java/com/superluminar/tutorials/client/EventClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Map;
 
-@FeignClient(value = "EVENTS", path = "/api")
+@FeignClient(value = "events", path = "/api")
 public interface EventClient {
 
     @PostMapping("/events")
